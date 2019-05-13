caaeffa0735bfde577f0e65a22458ab3f69dbc89
oscar <oscar.o@anvo.com>
2019-05-13 17:42:42 +0800

    元

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ function doCount($command)
       $actuallyPay =floatval($arr[2])/floatval($arr[3]);
       $numResult = sprintf('%.2f',
           ($actuallyPay-((floatval($arr[2]))/floatval($arr[4])))-($actuallyPay*0.004));
-      $systemMsg = "# Peso：".$arr[2]."元 \r\n";
+      $systemMsg = "# Peso：".$arr[2]."p \r\n";
       $systemMsg .= "# 话费设置汇率：".$arr[3]." \r\n";
       $systemMsg .= "# 人民币换Peso汇率：".$arr[4]." \r\n";
       $systemMsg .= "# 盈利：".$numResult."元 ";
